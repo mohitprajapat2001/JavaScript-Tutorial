f0b8b0d1ac04af427b7d69a04995b943a5130479
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2023-06-07 01:07:33 +0530

    Call Back

---
+++ Advance Js/callback.js
@@ -0,0 +1,18 @@
+const prompt=require("prompt-sync")({sigint:true});
+
+console.log('Callback Tutorial');
+// Synchronous Javascript
+// let a = prompt("hello enter your name ");
+// let b = prompt("hello enter your age ");
+// let c = prompt("hello what color you like ");
+// console.log("hello "+a+" your age is "+b+" and "+c+" is you favorite color");
+
+// Asynchronous Javascript
+// console.log("start");
+// setTimeout(()=>{
+//     console.log("Hello Bhai Kaise ho")
+// },3000)
+// console.log("end")
+
+// Callback Functions
+
