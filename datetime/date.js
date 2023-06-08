4d6d9cb24f3b05a1db0320839abdfe5c32f8d79f
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2023-06-09 02:10:36 +0530

    datetime

---
+++ datetime/date.js
@@ -0,0 +1,7 @@
+console.log("Date Function");
+// How to get Current Date
+let dt = new Date();
+console.log(dt)
+// let mydate = new Date(year, month, date, hours, minutes, second, millisecond)
+let mydate = new Date(2029, 06, 09, 5, 40, 30, 2000)
+console.log(mydate);
