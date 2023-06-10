e0a650187dc002b234fa3fbc259e11d34b02d410
Mohit Prajapat <mohitdevelopment2001@gmail.com>
2023-06-11 00:18:34 +0530

    trycatch

---
+++ Try Catch/trycatch.js
@@ -0,0 +1,33 @@
+setTimeout(() => {
+    console.log("Loading Channel")
+}, 1000)
+
+setTimeout(() => {
+    console.log("Subscribe Karolo")
+
+}, 3000)
+
+// Try Catch Statement
+// try {
+//     console.log(bharat)
+// } catch (err) {
+//     console.log(err)
+// }
+
+try {
+    setTimeout(() => {
+        try {
+            console.log("Video Dekhlo")
+            console.log(yash)
+        } catch (error) {
+
+        }
+    }, 5000)
+} catch (error) {
+    console.log(error)
+
+}
+
+setTimeout(() => {
+    console.log("Like Bhi Kardena Channel ko")
+}, 7000)
